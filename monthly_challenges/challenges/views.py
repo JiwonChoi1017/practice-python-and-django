226079ade415cbb0aeab8d899886b2a3258b2277
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-07-01 22:31:46 +0900

    テンプレート & 静的ファイルについて学習

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 monthly_challenges = {
@@ -13,23 +14,15 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!",
+    "december": None,
 }
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {"months": months})
 
 
 def monthly_challenge_by_number(request, month):
@@ -48,9 +41,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-
-        return HttpResponse(response_data)
+        return render(
+            request,
+            "challenges/challenge.html",
+            {"text": challenge_text, "month_name": month},
+        )
     # 明確な例外要因を記述することで、「do not use bare ‘except’」エラーを回避できる
     except Exception:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
