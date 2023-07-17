a9e1892b6e4a308bd7c6086a3442ab9b51829490
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-07-17 17:53:11 +0900

    フォーム作成方法について学習

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
+from .forms import ReviewForm
 
 # Create your views here.
 
 
-def review(request):
-    if request.method == "POST":
-        entered_username = request.POST["username"]
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
-    return render(request, "reviews/review.html")
+        return render(request, "reviews/review.html", {"form": form})
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+        return render(request, "reviews/review.html", {"form": form})
 
 
 def thank_you(request):
